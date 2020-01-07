d3068c9eed295f81cfd43ccf557f941c46f243e0
Rafael Noriega <rafaelnoriega0@gmail.com>
2020-01-06 16:28:16 -0800

    deleting api reference

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import Info from './Info'
 const App = () =>{
   const getWeather = async () => {
     //https://developer.accuweather.com/apis
-    const url = "http://dataservice.accuweather.com/currentconditions/v1/39054_PC?apikey=jHoDGPmF8EMulC2gLWEEy23iEgVPk5ex&language=en-us&details=false";
+    const APIKEY = 'jHoDGPmF8EMulC2gLWEEy23iEgVPk5ex';
+    const loacation = '39054_PC';
+    const url = `http://dataservice.accuweather.com/currentconditions/v1/{location}?apikey={APIKEY}&language=en-us&details=false`;
     
     let response = await Axios.get(url);
     const data = response['data'][0]['Temperature']['Imperial']['Value'];
